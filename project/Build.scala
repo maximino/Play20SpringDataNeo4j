3a55b3e3f425bd73153f75cfc1d7e5c0e78ba1c2
Max Salazar <m.s.lechuga@gmail.com>
2013-05-08 16:15:24 -0500

    Now works with 2.1.1
    Signed-off-by: Max Salazar <m.s.lechuga@gmail.com>

--- project/Build.scala
+++ project/Build.scala
@@ -12,15 +12,15 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "asm"    %    "asm"    %    "3.3.1",
+    "asm" % "asm" % "3.3.1",
     "org.springframework" % "spring-context" % "3.2.1.RELEASE",
-    "org.neo4j" % "neo4j" % "1.9.M04",  
-	"org.springframework.data"    %    "spring-data-neo4j"      %    "2.3.0.BUILD-SNAPSHOT"
+    "org.neo4j" % "neo4j" % "1.9.RC1",  
+	  "org.springframework.data" % "spring-data-neo4j" % "2.3.0.BUILD-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-  	  resolvers += "Spring releases" at "http://maven.springframework.org/release",
-      resolvers += "Spring Data snapshot" at "http://repo.springsource.org/snapshot/",
+  	resolvers += "Spring releases" at "http://maven.springframework.org/release",
+    resolvers += "Spring Data snapshot" at "http://repo.springsource.org/snapshot/",
 	  resolvers += "Neo4j" at "http://m2.neo4j.org/content/repositories/releases/" 
   )
 
